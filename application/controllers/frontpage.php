51fefb31b28beffb1d6f8aea327cb52c216541d3
MacBook <lenykoskey@yahoo.com>
2014-07-06 18:15:28 +0300

    Added twitter boostrap and completed first version of this

---
+++ application/controllers/frontpage.php
@@ -0,0 +1,71 @@
+<?php if (!defined('BASEPATH')) die();
+class Frontpage extends Main_Controller {
+
+   public function index()
+	{
+		$male = 0;
+		$female = 0;
+		$fbdata = array();
+		for ($i = 1; $i < 101; $i++) {
+			$url = 'http://graph.facebook.com/' . $i;
+			$data = $this->get_content($url);
+			$data = json_decode($data);
+
+			if (!empty($data -> error)) {
+				$fbdata[$i] = 'left_facebook';
+				//echo $i . ". seems to have deleted account or left facebook<br>";
+
+			} else {
+				if ($data->gender == 'male') {
+					$male = $male+1;
+					
+				}if( $data->gender == 'female'){
+					$female == $female+1;
+					
+				}
+				//lets get the profile picture
+				$fbdata[$i]['username'] = $data->username;
+				$pic_url = 'http://graph.facebook.com/'.$data->username.'/picture';
+				$pic = $this->get_content($pic_url);
+				
+				$fbdata[$i]['name'] = $data->name;
+				$fbdata[$i]['gender'] = $data->gender;
+				
+				
+				$fbdata[$i]['pic'] = $pic;
+				
+				
+				//echo $i . "." . $data -> name . "<br>Link to profile: http://facebook.com/".$data->username."<br>";
+
+			}
+
+		}
+	  
+	$fb = array();
+	$fb['male'] = $male;
+	$fb['female'] = $female;
+	$fb['people'] = $fbdata;
+	  
+      $this->load->view('include/header');
+	  // $this->view->fbdata = $fbdata;
+	  // $this->view->male = $male;
+	  // $this->view->female = $female;
+      $this->load->view('frontpage',$fb);
+      $this->load->view('include/footer');
+	}
+	
+	
+	
+	function get_content($URL) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $URL);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+   
+}
+
+/* End of file frontpage.php */
+/* Location: ./application/controllers/frontpage.php */
---
+++ application/views/frontpage.php
@@ -0,0 +1,61 @@
+<div class="container">
+   <div class="jumbotron">
+   	
+         <ul class="list-group" id="contact-list">
+         	<?php $data = $people;
+			//print_r($data);
+			//exit;
+         	
+         	foreach ($data as $key => $value) {
+         		
+         		if ($value == 'left_facebook') { ?>
+         			 <li class="list-group-item">
+                        <!-- <div class="col-xs-12 col-sm-3">
+                            <img src="http://api.randomuser.me/portraits/men/49.jpg" alt="Scott Stevens" class="img-responsive img-circle" />
+                        </div> -->
+                        <div class="col-xs-12 col-sm-9">
+                        	<span class="name">User ID: <?php echo $key; ?></span><br/>
+                            <span class="name">User seems to have left facebook</span><br/>
+                            <span class="name">Gender: Not Available</span><br/>
+                          
+                           
+                            <a href="http://facebook.com/<?php echo $key; ?>"> <button class="btn btn-info btn-block"><span class="fa fa-user"></span> View Profile </a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </li>
+					 
+				 <?php }else{
+				 	//print_r($value);
+					//exit;
+				 	// echo $value['username'];
+				// exit;
+				 	?>
+				 	
+				 	 <li class="list-group-item">
+                        <div class="col-xs-12 col-sm-1">
+                            <img src="http://graph.facebook.com/<?php echo $value['username']; ?>/picture" alt="Scott Stevens" class="img-responsive img-circle" />
+                        </div>
+                        <div class="col-xs-12 col-sm-9">
+                        	 <span class="name">User ID: <?php echo $key; ?></span><br/>
+                            <span class="name"><?php echo $value['name']; ?></span><br/>
+                            <span class="name">Gender: <?php echo $value['gender']; ?></span><br/>
+                           
+                           
+                            <a href="http://facebook.com/<?php echo $value['username']; ?>"> <button class="btn btn-info btn-block"><span class="fa fa-user"></span> View Profile </a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </li>
+				 <?php }
+				
+				 
+			 }
+         	
+         	?>
+                   
+                 
+                  
+                   
+                 
+                </ul>
+   </div>
+</div>
